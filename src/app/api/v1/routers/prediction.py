d05edd55267633227f76e724a9d7c13a7383208d
Enrico Shippole <enricoship@gmail.com>
2025-03-19 00:42:53 -0400

    update initial file structure

---
+++ src/app/api/v1/routers/prediction.py
@@ -0,0 +1,19 @@
+import modal
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+image = modal.Image.debian_slim().pip_install("fastapi[standard]", "boto3")
+app = modal.App(image=image)
+
+
+class Item(BaseModel):
+    name: str
+    qty: int = 42
+
+
+@app.function()
+@modal.fastapi_endpoint(method="POST")
+def f(item: Item):
+    import boto3
+    # do things with boto3...
+    return HTMLResponse(f"<html>Hello, {item.name}!</html>")
