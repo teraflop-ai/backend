6968bd0025bb309529d5b65abb33997e46ad9a66
Enrico Shippole <enricoship@gmail.com>
2025-04-21 00:29:53 -0400

    Updates

--- src/app/api/v1/routes/stripe.py
+++ src/app/api/v1/routes/stripe.py
@@ -1,3 +1,4 @@
+import stripe
 from stripe import Customer, checkout, error
 from app.secrets.infisical import (STRIPE_SECRET_KEY, STRIPE_PUBLISHABLE_KEY)
 
@@ -5,6 +6,21 @@ from fastapi import APIRouter, Depends
 
 payment_router = APIRouter(prefix="/payments", tags=["Payments"])
 
-@payment_router.get("")
-async def create_payments():
+@payment_router.get(
+    "", 
+    dependencies=[
+        Depends(get_current_user()),
+        Depends(get_db())
+    ]
+)
+def create_payments():
+    try:
+        checkout_session = checkout.Session.create(
+
+        )
+    except Exception as e:
+        return
+
+@payment_router.get("", dependencies=[])
+def webhook_recieved():
     pass
